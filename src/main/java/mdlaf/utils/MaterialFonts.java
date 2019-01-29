98803418339a20333a5f8769c1fbdb5151df176b
xuehongzhi <rssdingyue4@gmail.com>
2019-01-29 16:56:05 +0800

    增加中文字体

--- src/main/java/mdlaf/utils/MaterialFonts.java
+++ src/main/java/mdlaf/utils/MaterialFonts.java
@@ -11,25 +11,25 @@ import java.util.Map;
 public class MaterialFonts {
 
 	private static final Map<TextAttribute, Object> fontSettings = new HashMap<TextAttribute, Object> ();
-	public static final Font BLACK = loadFont ("/fonts/NotoSans-Black.ttf");
+	public static final Font BLACK = loadFont ("/fonts/NotoSansCJKsc-Black.ttf");
 	public static final Font BLACK_ITALIC = loadFont ("/fonts/NotoSans-BlackItalic.ttf");
-	public static final Font BOLD = loadFont ("/fonts/NotoSans-Bold.ttf");
+	public static final Font BOLD = loadFont ("/fonts/NotoSansCJKsc-Bold.ttf");
 	public static final Font BOLD_ITALIC = loadFont ("/fonts/NotoSans-BoldItalic.ttf");
 	public static final Font ITALIC = loadFont ("/fonts/NotoSans-Italic.ttf");
-	public static final Font LIGHT = loadFont ("/fonts/NotoSans-Light.ttf");
+	public static final Font LIGHT = loadFont ("/fonts/NotoSansCJKsc-Light.ttf");
 	public static final Font LIGHT_ITALIC = loadFont ("/fonts/NotoSans-LightItalic.ttf");
-	public static final Font MEDIUM = loadFont ("/fonts/NotoSans-Medium.ttf");
+	public static final Font MEDIUM = loadFont ("/fonts/NotoSansCJKsc-Medium.ttf");
 	public static final Font MEDIUM_ITALIC = loadFont ("/fonts/NotoSans-MediumItalic.ttf");
-	public static final Font REGULAR = loadFont ("/fonts/NotoSans-Regular.ttf");
+	public static final Font REGULAR = loadFont ("/fonts/NotoSansCJKsc-Regular.ttf");
 	public static final Font THIN = loadFont ("/fonts/NotoSans-Thin.ttf");
 	public static final Font THIN_ITALIC = loadFont ("/fonts/NotoSans-ThinItalic.ttf");
 
 	private static Font loadFont (String fontPath) {
 		if (fontSettings.isEmpty ()) {
-			fontSettings.put (TextAttribute.SIZE, 14f);
+			fontSettings.put (TextAttribute.SIZE, 12f);
 			fontSettings.put (TextAttribute.KERNING, TextAttribute.KERNING_ON);
 		}
-
+                
 		try (InputStream inputStream = MaterialFonts.class.getResourceAsStream (fontPath)) {
 			return Font.createFont (Font.TRUETYPE_FONT, inputStream).deriveFont (fontSettings);
 		}
